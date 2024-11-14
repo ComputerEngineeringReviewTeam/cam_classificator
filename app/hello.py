0467065415cfbc470b0bad300fcdb77ceb46eaf3
msz <marek.szymanski422@outlook.com>
2024-11-14 14:37:26 +0100

    removed dynamic forms

--- app/hello.py
+++ app/hello.py
@@ -3,11 +3,11 @@ import random
 
 from bson import ObjectId
 from flask import Flask, render_template, request
+import json
 
 from app.domain.training_data.queries.create_command import CreateTrainingDataCommand
 from app.domain.training_data.queries.update_command import UpdateTrainingDataCommand
 from app.domain.training_data.services import training_data_service
-from app.form_generator.generator import *
 
 app = Flask(__name__)
 
@@ -17,10 +17,6 @@ app.secret_key = 'my_secret_key'
 
 @app.route('/')
 def view_form():
-    form_config = load_config("form.json")
-    form = create_form(form_config)
-
-
     # id = training_data_service.create(
     #     CreateTrainingDataCommand().
     #              set_total_area(random.random()).
@@ -42,7 +38,7 @@ def view_form():
 
     print(json.dumps(training_data_service.get_all()))
 
-    return render_template('form.html', form_html=str(form))
+    return render_template('form.html')
 
 
 @app.route('/handle_post', methods=['POST'])
@@ -59,14 +55,7 @@ def handle_post():
             photo
         )
 
-        form_config = load_config("form.json")
-        form_results = request.form
-        typecasted_form_results = update_types(form_results, form_config)
-        # just to show that type are correct
-        form_content_str = ""
-        for k, v in typecasted_form_results.items():
-            form_content_str += f"<p>{k}={v} ({type(v).__name__})</p></br>"
-        return render_template('form_result.html', form_content=form_content_str, ok=True)
+        return render_template('form_result.html', ok=True)
     else:
         return render_template('form_result.html', ok=False)
 
