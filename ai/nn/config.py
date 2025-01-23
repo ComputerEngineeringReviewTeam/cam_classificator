fa62f0774c6c9c612033df59de5ec69c2a0fdb6e
Marek Szymański <115031525+m-szym@users.noreply.github.com>
2025-01-23 19:21:48 +0100

    Nn update dataloading (#22)
    * prepared cam_nn for training and testing
    
    * updated cam_nn.py and config.py

--- ai/nn/config.py
+++ ai/nn/config.py
@@ -1,8 +1,21 @@
 import torch
 
-LEARNING_RATE = 1e-3
+# Data config
+ROOT_DIR = "../../"
+TARGET_SIZE = (224, 224)
 BATCH_SIZE = 8
+
+# Device config
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-EPOCHS = 5
+
+# Training config
 TRAIN = False
+LEARNING_RATE = 1e-3
+EPOCHS = 5
 MODEL_NAME = "resnet50"
+
+# Test config
+TEST = False
+METRIC_TASK = 'multilabel'
+NUM_LABELS = 5
+
