b77617c54f37c519e02f87939b933c4a530fda73
Pawel Bogdanowicz <pawelb021@gmail.com>
2025-01-24 18:07:01 +0100

    fixed the scrapper

--- data-scrapper/scrape.py
+++ data-scrapper/scrape.py
@@ -37,13 +37,22 @@ def fetch_datapoint(datapoint_id: str) -> dict:
     """
     datapoint_url = BASE_URL + '/training_data/'
     datapoint_resp = session.get(datapoint_url + datapoint_id)
+
+    img_start = datapoint_resp.text.find('<img src="/cam/training_data/photos/')
+    img_end = datapoint_resp.text.find('"', img_start + len('<img src="/cam/training_data/photos/'))
+    img_full = datapoint_resp.text[img_start + len('<img src="/cam/training_data/photos/'):img_end]
+    img = os.path.basename(img_full)
+
     result = {
+        'id': datapoint_id,
         'created_at': scrape_datapoint_value(datapoint_resp.text, 'Created at'),
         'total_area': float(scrape_datapoint_value(datapoint_resp.text, 'Total area')),
+        'total_length': float(scrape_datapoint_value(datapoint_resp.text, 'Total length')),
         'mean_thickness': float(scrape_datapoint_value(datapoint_resp.text, 'Mean thickness')),
         'branching_points': float(scrape_datapoint_value(datapoint_resp.text, 'Branching points')),
         'is_good': scrape_datapoint_value(datapoint_resp.text, 'Is the tissue good'),
         'scale': scrape_datapoint_value(datapoint_resp.text, 'Photo scale'),
+        'img': img
     }
 
     if result['is_good'] == 'N/a':
@@ -89,24 +98,23 @@ def login(key: str) -> bool:
     return True
 
 
-def download_photos(data_ids) -> None:
+def download_photos(data) -> None:
     """
     Download photos from the website and save them to the photo directory.
     """
     photo_url = BASE_URL + '/training_data/photos/'
-    for data_id in data_ids:
-        for ext in ['.JPG', '.jpg', '.JPEG', '.jpeg', '.PNG', '.png']:
-            url = photo_url + data_id + ext
-            response = session.get(url, stream=True)
-            if response.status_code != 200:
-                continue
-            response.raise_for_status()
-            filename = os.path.join(SAVE_DIR + 'photos', os.path.basename(url))
+    for single_data in data.values():
+        url = photo_url + single_data['img']
+        response = session.get(url, stream=True)
+        if response.status_code != 200:
+            continue
+        response.raise_for_status()
+        filename = os.path.join(SAVE_DIR + 'photos', os.path.basename(url))
 
-            with open(filename, "wb") as f:
-                for chunk in response.iter_content(1024):
-                    f.write(chunk)
-            print(f"Downloaded: {filename}")
+        with open(filename, "wb") as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+        print(f"Downloaded: {filename}")
 
 
 def main():
@@ -128,7 +136,7 @@ def main():
         json.dump(data, file, indent=4)
 
     print('Downloaded data. Starting downloading photos...\n')
-    download_photos(data.keys())
+    download_photos(data)
 
 
 if __name__ == "__main__":
