1299dab09e4b833b7c49a0597d737254ba200250
msz <jek_14@wp.pl>
2025-06-07 10:20:52 +0200

    update CamNetConv to standard api

--- ai/model/camnet_conv.py
+++ ai/model/camnet_conv.py
@@ -5,7 +5,7 @@ class CamNetConv(torch.nn.Module):
     def __init__(self):
         super(CamNetConv, self).__init__()
 
-        self.regressor = torch.nn.Sequential(
+        self.extractor = torch.nn.Sequential(
             # 224 x 224 x 3
             torch.nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=0),
             torch.nn.LeakyReLU(),
@@ -33,7 +33,7 @@ class CamNetConv(torch.nn.Module):
             # 21 x 21 x 3
         )
 
-        self.end = torch.nn.Sequential(
+        self.regressor = torch.nn.Sequential(
             torch.nn.Linear(in_features=1323, out_features=256),
             torch.nn.LeakyReLU(),
             torch.nn.Linear(in_features=256, out_features=64),
@@ -44,9 +44,8 @@ class CamNetConv(torch.nn.Module):
 
 
 
-    def forward(self, image):
-        regression_output = self.regressor(image)
-        # print(regression_output.shape)
-        regression_output = self.end(regression_output.flatten(start_dim=1))
-
+    def forward(self, inputs):
+        image, scale = inputs
+        extracted_features = self.extractor(image)
+        regression_output = self.regressor(extracted_features.flatten(start_dim=1))
         return regression_output
